a47183f26c105a29b1d65bdaaddf3e3ce18fc160
Debasish Debnath <ddebnath241@gmail.com>
2025-04-16 13:55:50 +0530

    port update

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello from EC2 Node.js!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
